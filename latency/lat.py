a38684917385b652d36a2f893a824ed671cfbba1
bobby096310 <chiuchienhao@outlook.com>
2023-09-19 16:52:42 +0000

    add prewarm TF

--- latency/lat.py
+++ latency/lat.py
@@ -10,6 +10,8 @@ def execute(func, data):
     headers = {"Content-Type": "application/json; charset=utf-8", "Host": func + ".default.svc.cluster.local"}
     start_time = datetime.now()
     resp = requests.post(url, headers=headers, json=data)
+    if 'dummy' in data:
+        return
     while (not resp.ok):
         print("Retry")
         time.sleep(0.5)
@@ -22,6 +24,8 @@ def execute(func, data):
     return result, lat
 
 def execute_map(func, data, out_list, lat_list):
+    if 'dummy' in data:
+        return
     exec_result, lat = execute(func, data)
     out_list.append(exec_result)
     lat_list.append(lat)
@@ -40,6 +44,8 @@ def map(func, input_raw, timeout=None):
     for t in ths:
         (p, a) = t
         p.join()
+    if (len(out_list) == 0):
+        return
     return list(out_list), max(list(lat_list))
     '''
     ths2 = []
@@ -70,12 +76,23 @@ def run_step(func, data):
     else:
         return execute(func, data)
 
-def run_video(inp):
+def pre_warm(func, width):
+    dummy = {"detail":{"indeces":["{'dummy': 1}" for i in range(width)]}}
+    run_step(func, dummy)
+
+def run_video(inp, prewarm):
     latencies = []
     start_time = datetime.now()
+    width = int(inp['DOP'])
 
     latencies.append("split")
     re1, lat1 = run_step('vi-split', inp)
+    
+    if prewarm:
+        pre_warm('vi-extract', width)
+        pre_warm('vi-shuffle', 1)
+        pre_warm('vi-classify', width)
+    
     latencies.append(str(lat1))
 
     latencies.append("extract")
@@ -94,9 +111,30 @@ def run_video(inp):
     latencies.append("E2E {:d}".format(int((end_time-start_time).total_seconds()*1000)))
     return (" ".join(latencies))
 
+def run_ml(inp):
+    latencies = []
+    start_time = datetime.now()
+    width = int(inp['bundle_size'])
+
+    latencies.append("pca")
+    re1, lat1 = run_step('ml-pca', inp)
+    latencies.append(str(lat1))
+
+    latencies.append("param-tune")
+    re2, lat2 = run_step('ml-param-tune', re1)
+    latencies.append(str(lat2))
+
+    latencies.append("combine")
+    re3, lat3 = run_step('ml-combine', re2)
+    latencies.append(str(lat3))
+
+    end_time = datetime.now()
+    latencies.append("E2E {:d}".format(int((end_time-start_time).total_seconds()*1000)))
+    return (" ".join(latencies))
+
 def main():
     inp = {"src_name": "0", "DOP": "30", "detect_prob": 2}
-    print(run_video(input))
+    print(run_video(inp))
 
 if __name__ == "__main__":
     main()
--- latency/test2.py
+++ latency/test2.py
@@ -15,18 +15,18 @@ def create_random_name(n):
     profile_hash = ''.join(random.choice(letters) for i in range(n))
     return profile_hash
 
-def run_workflows(namespace, workflow_name, filename, times, inp, log_file_name):
+def run_workflows(namespace, workflow_name, filename, times, inp, pre_warm, log_file_name):
     results = {}
     for i in range(int(times)):
         #run_name, result = run_workflow(namespace, filename)
         run_name = workflow_name + "-" + create_random_name(5)
-        results[run_name] = run_video(inp)
+        results[run_name] = run_video(inp, pre_warm)
         with open("logs2/" + log_file_name, 'a') as file:
             file.write(run_name + " " + results[run_name] + "\n")
         time.sleep(30)
     return results
     
-def run(workflow_name, function_index, conc_index, cpu_index, timeout_index, times, inp):
+def run(workflow_name, function_index, conc_index, cpu_index, timeout_index, times, inp, pre_warm):
     log_file_name = workflow_name + "_" + str(function_index) + "_" + str(conc_index) + "_" + str(cpu_index) + "_" + str(timeout_index) + "_" + create_random_name(5) + ".txt"
     filename = filenames[workflow_name]
     spec = {}
@@ -37,20 +37,13 @@ def run(workflow_name, function_index, conc_index, cpu_index, timeout_index, tim
     if cpu_index != '-':
         spec['cpu'] = cpu[cpu_index]
     update(filename, workflow_functions[workflow_name][function_index], spec)
-    result = run_workflows('argo-wf', workflow_name, filename, times, inp, log_file_name)
+    result = run_workflows('argo-wf', workflow_name, filename, times, inp, pre_warm, log_file_name)
     final = log_file_name[:-4] + " " +  analyze_result(list(result.values()), 1)
     return final
 
 def main():
     inp = {"src_name": "0", "DOP": "30", "detect_prob": 2}
-    pprint(run("video", 3, 1, 3, '-', 10, inp))
-    pprint(run("video", 3, 2, 3, '-', 10, inp))
-    pprint(run("video", 3, 1, 2, '-', 10, inp))
-    pprint(run("video", 3, 2, 2, '-', 10, inp))
-    pprint(run("video", 3, 1, 1, '-', 10, inp))
-    pprint(run("video", 3, 2, 2, '-', 10, inp))
-    pprint(run("video", 3, 1, 0, '-', 10, inp))
-    pprint(run("video", 3, 2, 0, '-', 10, inp))
+    pprint(run('video', 3, 2, 0, '-', 3, inp, True)) 
 
 if __name__ == "__main__":
     main()
