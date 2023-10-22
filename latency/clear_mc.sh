7d3e4b3fcbeae1978f5b672bfcf7475047a2d86e
bobby096310 <chiuchienhao@outlook.com>
2023-10-22 20:24:18 +0000

    modify CPU

--- latency/clear_mc.sh
+++ latency/clear_mc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for (( i=0; i<10; i++))
+for (( i=0; i<100; i++))
 do
 	mc rm --recursive --force  myminio/bucket1/Video_Frames_Step/ --older-than 10m
 	mc rm --recursive --force  myminio/bucket1/Video_Chunks_Step/ --older-than 10m
--- latency/test.py
+++ latency/test.py
@@ -10,7 +10,8 @@ filenames = {"ml": "../argo/ml.yaml", "video": "../argo/video.yaml"}
 workflow_functions = {"ml": ["ml-pca","ml-param-tune","ml-combine"], "video": ["vi-split", "vi-extract", "vi-shuffle", "vi-classify"]}
 conc = [0, 1, 2, 5, 10]
 #cpu = [['-', '-'], ['1', '-'], ['1', '2'], ['1500m', '-'], ['2', '-'], ['2', '3'], ['3', '-']]
-cpu = [['-', '-'], ['500m', '500m'], ['1', '1'], ['1500m', '1500m'], ['2', '2'], ['2500m', '2500m'], ['3', '3']]
+#cpu = [['-', '-'], ['500m', '500m'], ['1', '1'], ['1500m', '1500m'], ['2', '2'], ['2500m', '2500m'], ['3', '3']]
+cpu = [['-', '-'], ['1', '1'], ['2', '2'], ['3', '3']]
 bundle = {"ml": ["2", "4", "8"], "video": ["30", "15", "5", "3"]}
 
 def create_random_name(n):
@@ -19,14 +20,21 @@ def create_random_name(n):
     return profile_hash
 
 # Call the runner to run the workflow based on name
-def run_workflows(namespace, workflow_name, filename, times, inp, pre_warm, log_file_name):
+def run_workflows(workflow_name, filename, times, inp, pre_warm, log_file_name):
     results = {}
+    if pre_warm == 'T':
+        pre_warm_value = True
+    elif pre_warm == 'F':
+        pre_warm_value = False
+    else:
+        print("Pre_warm string invalid.")
+        return
     for i in range(int(times)):
         run_name = workflow_name + "-" + create_random_name(5)
         if workflow_name == "video":
-            results[run_name] = run_video(inp, pre_warm)
+            results[run_name] = run_video(inp, pre_warm_value)
         elif workflow_name == 'ml':
-            results[run_name] = run_ml(inp, pre_warm)
+            results[run_name] = run_ml(inp, pre_warm_value)
         else:
             return "Workflow not found!"
         with open("logs/" + log_file_name, 'a') as file:
@@ -40,7 +48,7 @@ def update_and_run(workflow_name, config, times, pre_warm):
     conc_index = config[1]
     cpu_index = config[2]
     bundle_index = int(config[3])
-    log_file_name = workflow_name + "_" + str(function_index) + "_" + str(conc_index) + "_" + str(cpu_index) + "_" + str(bundle_index) + "_" + ('T' if pre_warm else 'F') + "_" + create_random_name(5) + ".txt"
+    log_file_name = workflow_name + "_" + str(function_index) + "_" + str(conc_index) + "_" + str(cpu_index) + "_" + str(bundle_index) + "_" + pre_warm + "_" + create_random_name(5) + ".txt"
     filename = filenames[workflow_name]
     spec = {}
     if conc_index != '-':
@@ -48,8 +56,9 @@ def update_and_run(workflow_name, config, times, pre_warm):
     if cpu_index != '-':
         spec['cpu'] = cpu[int(cpu_index)]
     update_function(workflow_functions[workflow_name][function_index], spec)
-    inp = {"src_name": "0", "DOP": bundle[workflow_name][bundle_index], "detect_prob": 2}
-    result = run_workflows('argo-wf', workflow_name, filename, times, inp, pre_warm, log_file_name)
+    if workflow_name == 'video' :
+        inp = {"src_name": "0", "DOP": bundle[workflow_name][bundle_index], "detect_prob": 2}
+    result = run_workflows(workflow_name, filename, times, inp, pre_warm, log_file_name)
     final = log_file_name[:-4] + " " +  analyze_result(list(result.values()), 7)
     #get_n_latency(list(result.values()), 95)
     return final
@@ -59,14 +68,14 @@ def update_and_run(workflow_name, config, times, pre_warm):
     #pprint(run('video', [3, 1, 0, 1], 10, False))
 
 # Run each possible spec once
-def init(workflow_name, func_index, conc, resources, bundling):
+def init(workflow_name, func_index, conc, resources, bundling, pre_warm):
     current_list = [r.split(" ")[0] for r in get_P50()]
     for r in range(len(resources)):
         for b in range(len(bundling)):
-            entry = workflow_name + "_" + str(func_index) + "_" + str(conc) + "_" + str(r) + "_" + str(b) +  "_F"
+            entry = workflow_name + "_" + str(func_index) + "_" + str(conc) + "_" + str(r) + "_" + str(b) +  "_" + pre_warm
             if entry not in current_list:
                 print("run " + entry)
-                update_and_run(workflow_name, [func_index, 1, r, b], 1, False)
+                update_and_run(workflow_name, [func_index, 1, r, b], 1, pre_warm)
                 time.sleep(60)
 
 # Run workflows until target is met for the level
@@ -84,7 +93,7 @@ def run_from_str(input_str, times):
     config_str = input_str.split(" ")[0]
     config_list = config_str.split("_")
     workflow_name = config_list[0]
-    update_and_run(workflow_name, [int(config_list[1]), int(config_list[2]), int(config_list[3]), int(config_list[4])], times, False if config_list[5] == 'F' else True)
+    update_and_run(workflow_name, [int(config_list[1]), int(config_list[2]), int(config_list[3]), int(config_list[4])], times, config_list[5])
     
 def get_P50():
     groupnames = fetch_groups()
@@ -108,9 +117,9 @@ def get_best_config():
             return config
     return "NaN"
 
-def find_best_config(workflow_name, func_index):
+def find_best_config(workflow_name, func_index, pre_warm):
     conc = 1
-    init(workflow_name, func_index, conc, cpu, bundle[workflow_name])
+    init(workflow_name, func_index, conc, cpu, bundle[workflow_name], pre_warm)
     level = 0
     while level < 20:
         batch, target = get_next_level(level, 10)
@@ -122,12 +131,12 @@ def find_best_config(workflow_name, func_index):
         level += 1
     print(get_best_config())
 
-def run_all(workflow_name, func_index):
+def run_all(workflow_name, func_index, rounds, pre_warm):
     conc = 1
-    init(workflow_name, func_index, conc, cpu, bundle[workflow_name])
+    init(workflow_name, func_index, conc, cpu, bundle[workflow_name], pre_warm)
     batch = get_P50()
-    level = 1
-    while level < 11:
+    level = 0
+    while level < int(rounds):
         print("level: ", level)
         run_level(batch, level * 10)
         level += 1
@@ -138,11 +147,12 @@ def main():
     #interval = int(args[0])
     code = args[0]
     if code == "best":
-        if len(args) < 3:
-            print("Please enter workflow name and function index")
+        if len(args) < 4:
+            print("Please enter workflow name, function index, and pre_warm")
         workflow_name = args[1]
         func_index = args[2]
-        find_best_config(workflow_name, func_index)
+        pre_warm = args[3]
+        find_best_config(workflow_name, func_index, pre_warm)
     elif code == "run":
         if len(args) < 8:
             print("Please enter workflow name, function index, concurrency, cpu, bundle, pre_warm, and times")
@@ -151,16 +161,18 @@ def main():
         c = args[3]
         r = args[4]
         b = args[5]
-        pre_warm = False if args[6] == 'F' else True
+        pre_warm = args[6]
         times = args[7]
         update_and_run(workflow_name, [func_index, c, r, b], times, pre_warm)
         #python3 test2.py run video 3 1 - 0 F 10
     elif code == "run_all":
-        if len(args) < 3:
-            print("Please enter workflow name and function index")
+        if len(args) < 4:
+            print("Please enter workflow name, function index, pre_warm, and rounds")
         workflow_name = args[1]
         func_index = args[2]
-        run_all(workflow_name, func_index)
+        pre_warm = args[3]
+        rounds = args[4]
+        run_all(workflow_name, func_index, rounds, pre_warm)
 
 
 if __name__ == "__main__":
