19052281c2ff9651c99854141ccef381a93732eb
bobby096310 <chiuchienhao@outlook.com>
2023-10-29 23:49:51 +0000

    more data

--- latency/git.sh
+++ latency/git.sh
@@ -3,7 +3,7 @@ for (( i=0; i<100; i++))
 do
 	git add ./
 	git commit -m "more data"
-      	git push https://bobby096310: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@github.com/bobby096310/dag-workflow-optimization-k8s  	
+      	git push https://bobby096310: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@github.com/bobby096310/dag-workflow-optimization-k8s  	
 
 sleep 600
 done
