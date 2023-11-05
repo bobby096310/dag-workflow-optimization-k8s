16eee9bce64ffb2c739e0de3d5063dee15d8da35
bobby096310 <chiuchienhao@outlook.com>
2023-11-05 11:56:28 +0000

    more data

--- latency/run_10.sh
+++ latency/run_10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 test.py run ml 1 1 3 3 T 10
-python3 test.py run ml 1 1 3 3 S 10
-python3 test.py run ml 1 1 3 3 F 10
+python3 test.py run ml 1 1 3 0 T 10
+python3 test.py run ml 1 1 3 0 S 10
+python3 test.py run ml 1 1 3 0 F 10
           
